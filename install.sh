0a8350819fee572e4f93f3b95b8ac7b839e419cd
vip-repetitor <73946677+vip-repetitor@users.noreply.github.com>
2022-09-22 03:08:44 +0300

    upd

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-base_python_interpreter=""
+base_python_interpreter="/home/www/.python3.8"
 project_domain=""
 project_path=`pwd`
 
-read -p "Python interpreter: " base_python_interpreter
+base_python_interpreter
 read -p "Your domain without protocol (for example, google.com): " project_domain
 `$base_python_interpreter -m venv env`
 source env/bin/activate
